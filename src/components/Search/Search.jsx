3635ebb44ddc3808816532fefb9f5b2199e5dde9
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-05-28 07:14:10 +0300

    upgrade working with Search and fix styles

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useCallback, useContext, useRef, useState } from 'react'
 
 import styles from './Search.module.scss'
 import { SearchContext } from '../../App'
+import debounce from 'lodash.debounce'
 
 const Search = () => {
-  const { searchValue, setSearchValue } = useContext(SearchContext)
+  const [value, setValue] = useState('')
+  const { setSearchValue } = useContext(SearchContext)
+  const inputRef = useRef()
+
+  const onInputChanged = (e) => {
+    const str = e.target.value
+    setValue(str)
+    updateSearchValue(str)
+  }
+
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      setSearchValue(str)
+    }, 1000),
+    []
+  )
+
+  function onInputCleared() {
+    setSearchValue('')
+    setValue('')
+    inputRef.current.focus()
+  }
+
   return (
     <div className={styles.root}>
       <svg
@@ -21,13 +44,13 @@ const Search = () => {
         <circle cx='11' cy='11' r='8' />
         <line x1='21' x2='16.65' y1='21' y2='16.65' />
       </svg>
-      {searchValue && (
+      {value && (
         <svg
           className={styles.clearIcon}
           enableBackground='new 0 0 32 32'
           height='32px'
           viewBox='0 0 32 32'
-          onClick={() => setSearchValue('')}>
+          onClick={onInputCleared}>
           <path
             d='M17.459,16.014l8.239-8.194c0.395-0.391,0.395-1.024,0-1.414c-0.394-0.391-1.034-0.391-1.428,0  l-8.232,8.187L7.73,6.284c-0.394-0.395-1.034-0.395-1.428,0c-0.394,0.396-0.394,1.037,0,1.432l8.302,8.303l-8.332,8.286  c-0.394,0.391-0.394,1.024,0,1.414c0.394,0.391,1.034,0.391,1.428,0l8.325-8.279l8.275,8.276c0.394,0.395,1.034,0.395,1.428,0  c0.394-0.396,0.394-1.037,0-1.432L17.459,16.014z'
             fill='#121313'
@@ -42,10 +65,12 @@ const Search = () => {
         </svg>
       )}
       <input
+        ref={inputRef}
         className={styles.input}
-        value={searchValue}
+        value={value}
         placeholder='Поиск пиццы...'
-        onChange={(e) => setSearchValue(e.target.value)}
+        // onChange={(e) => setSearchValue(e.target.value)}
+        onChange={onInputChanged}
       />
     </div>
   )
